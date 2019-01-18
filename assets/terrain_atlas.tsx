d225146810e3e535a3d83b244952e68bad107169
AllisonAI <alexbrowses@icloud.com>
2019-01-17 21:22:15 -0500

    Add files via upload

---
+++ assets/terrain_atlas.tsx
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="terrain_atlas" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="terrain_atlas.png" width="1024" height="1024"/>
+ <terraintypes>
+  <terrain name="New Terrain" tile="391"/>
+  <terrain name="New Terrain" tile="118"/>
+  <terrain name="New Terrain" tile="112"/>
+  <terrain name="New Terrain" tile="929"/>
+  <terrain name="New Terrain" tile="737"/>
+  <terrain name="New Terrain" tile="554"/>
+  <terrain name="New Terrain" tile="932"/>
+ </terraintypes>
+ <tile id="16" terrain="2,2,2,"/>
+ <tile id="17" terrain="2,2,,2"/>
+ <tile id="22" terrain="1,1,1,"/>
+ <tile id="23" terrain="1,1,,1"/>
+ <tile id="48" terrain="2,,2,2"/>
+ <tile id="49" terrain=",2,2,2"/>
+ <tile id="54" terrain="1,,1,1"/>
+ <tile id="55" terrain=",1,1,1"/>
+ <tile id="79" terrain=",,,2"/>
+ <tile id="80" terrain=",,2,2"/>
+ <tile id="81" terrain=",,2,"/>
+ <tile id="85" terrain=",,,1"/>
+ <tile id="86" terrain=",,1,1"/>
+ <tile id="87" terrain=",,1,"/>
+ <tile id="111" terrain=",2,,2"/>
+ <tile id="112" terrain="2,2,2,2"/>
+ <tile id="113" terrain="2,,2,"/>
+ <tile id="117" terrain=",1,,1"/>
+ <tile id="118" terrain="1,1,1,1"/>
+ <tile id="119" terrain="1,,1,"/>
+ <tile id="143" terrain=",2,,"/>
+ <tile id="144" terrain="2,2,,"/>
+ <tile id="145" terrain="2,,,"/>
+ <tile id="149" terrain=",1,,"/>
+ <tile id="150" terrain="1,1,,"/>
+ <tile id="151" terrain="1,,,"/>
+ <tile id="298" terrain="0,0,0,"/>
+ <tile id="299" terrain="0,0,,0"/>
+ <tile id="330" terrain="0,,0,0"/>
+ <tile id="331" terrain=",0,0,0"/>
+ <tile id="361" terrain=",,,0"/>
+ <tile id="362" terrain=",,0,0"/>
+ <tile id="363" terrain=",,0,"/>
+ <tile id="393" terrain=",0,,0"/>
+ <tile id="394" terrain="0,0,0,0"/>
+ <tile id="395" terrain="0,,0,"/>
+ <tile id="425" terrain=",0,,"/>
+ <tile id="426" terrain="0,0,,"/>
+ <tile id="427" terrain="0,,,"/>
+ <tile id="521" terrain=",,,5"/>
+ <tile id="522" terrain=",,5,5"/>
+ <tile id="523" terrain=",,5,"/>
+ <tile id="553" terrain=",5,,5"/>
+ <tile id="554" terrain="5,5,5,5"/>
+ <tile id="555" terrain="5,,5,"/>
+ <tile id="585" terrain=",5,,5"/>
+ <tile id="586" terrain="5,5,5,5"/>
+ <tile id="587" terrain="5,,5,"/>
+ <tile id="617" terrain=",5,,"/>
+ <tile id="618" terrain="5,5,,"/>
+ <tile id="619" terrain="5,,,"/>
+ <tile id="641" terrain="4,4,4,"/>
+ <tile id="642" terrain="4,4,,4"/>
+ <tile id="673" terrain="4,,4,4"/>
+ <tile id="674" terrain=",4,4,4"/>
+ <tile id="678" terrain="5,5,5,"/>
+ <tile id="679" terrain="5,5,,5"/>
+ <tile id="704" terrain=",,,4"/>
+ <tile id="705" terrain=",,4,4"/>
+ <tile id="706" terrain=",,4,"/>
+ <tile id="710" terrain="5,,5,5"/>
+ <tile id="711" terrain=",5,5,5"/>
+ <tile id="736" terrain=",4,,4"/>
+ <tile id="737" terrain="4,4,4,4"/>
+ <tile id="738" terrain="4,,4,"/>
+ <tile id="768" terrain=",4,,"/>
+ <tile id="769" terrain="4,4,,"/>
+ <tile id="770" terrain="4,,,"/>
+ <tile id="833" terrain="3,3,3,"/>
+ <tile id="834" terrain="3,3,,3"/>
+ <tile id="836" terrain="6,6,6,"/>
+ <tile id="837" terrain="6,6,,6"/>
+ <tile id="865" terrain="3,,3,3"/>
+ <tile id="866" terrain=",3,3,3"/>
+ <tile id="868" terrain="6,,6,6"/>
+ <tile id="869" terrain=",6,6,6"/>
+ <tile id="896" terrain=",,,3"/>
+ <tile id="897" terrain=",,3,3"/>
+ <tile id="898" terrain=",,3,"/>
+ <tile id="899" terrain=",,,6"/>
+ <tile id="900" terrain=",,6,6"/>
+ <tile id="901" terrain=",,6,"/>
+ <tile id="928" terrain=",3,,3"/>
+ <tile id="929" terrain="3,3,3,3"/>
+ <tile id="930" terrain="3,,3,"/>
+ <tile id="931" terrain=",6,,6"/>
+ <tile id="932" terrain="6,6,6,6"/>
+ <tile id="933" terrain="6,,6,"/>
+ <tile id="960" terrain=",3,,"/>
+ <tile id="961" terrain="3,3,,"/>
+ <tile id="962" terrain="3,,,"/>
+ <tile id="963" terrain=",6,,"/>
+ <tile id="964" terrain="6,6,,"/>
+ <tile id="965" terrain="6,,,"/>
+</tileset>
